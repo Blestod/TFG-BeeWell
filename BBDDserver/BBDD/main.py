d0e238a09112f00dc60ab00562bb2b62caf3c46e
Blestod <thomasdegroot1000@gmail.com>
2025-04-04 00:57:31 +0200

    BBDDtestrequest

---
+++ BBDDserver/BBDD/main.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from tables import db, User, Meal, Ingredient, Vital
+from flask import request
+
+api: Flask=Flask(__name__)
+api.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://thomas:root@localhost:3306/mydb"
+db.app=api
+db.init_app(api)
+
+@api.route("/api")
+def get_home():
+    return "Running serVidor"
+
+
+
+#---------------------USER-----------------------
+
+@api.route("/user", methods=["POST"])
+def post_user():
+    user = User(
+        email=request.json["email"],
+        password=request.json["password"],
+        birth_date=request.json["birth_date"],
+        sex=request.json["sex"],
+        insulin_type=request.json["insulin_type"]
+    )
+    db.session.add(user)
+    db.session.commit()
+    return "ok", 200
+
+#---------------------VITAL----------------------
+
+@api.route("/vital", methods=["POST"])
+def post_vital():
+    user = User.query.get(request.json["user_email"])
+    if user is None:
+        return "User not found", 404
+    vital = Vital(
+        user_email=user.email,
+        vital_time=request.json["vital_time"],
+        glucose_value=request.json["glucose_value"],
+        heart_rate=request.json["heart_rate"],
+        temperature=request.json["temperature"],
+        calories=request.json["calories"],
+        diastolic=request.json["diastolic"],
+        systolic=request.json["systolic"],
+        is_sleeping=request.json["is_sleeping"]
+    )
+    db.session.add(vital)
+    db.session.commit()
+    return "ok", 201
+
+@api.route("/vital/<int:vital_id>", methods=["GET"])
+def get_vital_by_id(vital_id:int):
+    vital = Vital.query.get(vital_id)
+    if vital is None:
+        return "Vital not found", 404
+    return vital.serialize(), 200
+
+#---------------------MAIN----------------------
+if __name__ == "__main__":
+    api.run()
--- BBDDserver/BBDD/tables.py
+++ BBDDserver/BBDD/tables.py
@@ -9,12 +9,28 @@ class User(db.Model):
     password = db.Column(db.String(60))
     birth_date = db.Column(db.Integer,nullable=True)
     sex = db.Column(db.Boolean,nullable=True)
-    insuline_type = db.Column(db.String(45),nullable=True)
+    insulin_type = db.Column(db.String(45),nullable=True)
+
     #db.relationship("nombre de la tabla","backref = x->crea una variable en tabla hijo que se llame x y que referencie al padre para meal.user o user.meal","al eliminar usuario se borran hijos")
-    meals = db.relationship("meal",backref="user",cascade="all,delete")
-    glucose_readings = db.relationship("glucose_reading",backref="user",cascade="all,delete")
-    vitals = db.relationship("vital",backref="user",cascade="all,delete")
-    predictions = db.relationship("prediction",backref="user",cascade="all,delete")
+    meals = db.relationship("Meal",backref="user",cascade="all,delete")
+    vitals = db.relationship("Vital",backref="user",cascade="all,delete")
+    predictions = db.relationship("Prediction",backref="user",cascade="all,delete")
+
+    def __init__(self, email, password, birth_date, sex, insulin_type):
+        self.email = email
+        self.password = password
+        self.birth_date = birth_date
+        self.sex = sex
+        self.insulin_type = insulin_type
+
+    def serialize(self):
+        return {
+            "email": self.email,
+            "password": self.password,
+            "birth_date": self.birth_date,
+            "sex": self.sex,
+            "insulin_type": self.insulin_type
+            }
 
 class Meal(db.Model):
     __tablename__="meal"
@@ -24,6 +40,7 @@ class Meal(db.Model):
     description = db.Column(db.String(300))
 
     user_email=db.Column(db.String(45),db.ForeignKey("user.email"))
+    ingredients = db.relationship("Ingredient",backref="meal",cascade="all,delete")
 
 class Ingredient(db.Model):
     __tablename__="ingredient"
@@ -35,24 +52,14 @@ class Ingredient(db.Model):
     estim_protein= db.Column(db.Float)
     estim_fats= db.Column(db.Float)
 
-    ingredients = db.relationship("ingredient",backref="meal",cascade="all,delete")
-
     user_email=db.Column(db.String(45),db.ForeignKey("user.email"))
     meal_meal_id=db.Column(db.Integer,db.ForeignKey("meal.meal_id"))
-
-class GlucoseReading(db.Model):
-    __tablename__="glucose_reading"
-
-    reading_id = db.Column(db.Integer,primary_key = True, autoincrement = True)
-    glucose_time = db.Column(db.Integer)
-    glucose_value = db.Column(db.Float)
-
-    user_email=db.Column(db.String(45),db.ForeignKey("user.email"))
 
 class Vital(db.Model):
     __tablename__="vital"
 
     vital_id = db.Column(db.Integer,primary_key = True, autoincrement = True)
+    glucose_value = db.Column(db.Float)
     vital_time = db.Column(db.Integer)
     heart_rate = db.Column(db.Float,nullable=True)
     temperature = db.Column(db.Float,nullable=True)
@@ -62,6 +69,29 @@ class Vital(db.Model):
     is_sleeping = db.Column(db.Boolean,nullable=True)
 
     user_email=db.Column(db.String(45),db.ForeignKey("user.email"))
+
+    def __init__(self, user_email, vital_time, glucose_value, heart_rate, temperature, calories, diastolic, systolic, is_sleeping):
+        self.vital_time = vital_time
+        self.user_email = user_email
+        self.glucose_value = glucose_value
+        self.heart_rate = heart_rate
+        self.temperature = temperature
+        self.calories = calories
+        self.diastolic = diastolic
+        self.systolic = systolic
+        self.is_sleeping = is_sleeping
+
+    def serialize(self):
+        return {
+            "vital_time": self.vital_time,
+            "glucose_value": self.glucose_value,
+            "heart_rate": self.heart_rate,
+            "temperature": self.temperature,
+            "calories": self.calories,
+            "diastolic": self.diastolic,
+            "systolic": self.systolic,
+            "is_sleeping": self.is_sleeping
+        }
 
 class Prediction(db.Model):
     __tablename__="prediction"
